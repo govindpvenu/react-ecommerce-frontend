797062c9a38863e5b3ec435f1dfebf53ce8979d2
govindpvenu <govindpvenu.txt@gmail.com>
2025-08-13 12:06:03 +0530

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import { BrowserRouter, NavLink, Route, Routes } from "react-router";
+import { BrowserRouter, Link, NavLink, Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+import Products from "./pages/Products";
+import { CircleUserRound, Handbag, ShoppingBag } from "lucide-react";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: "1rem", background: "#eee" }}>
-        <NavLink to="/" style={{ marginRight: "1rem" }}>
-          Home
-        </NavLink>
-        <NavLink to="/about">About</NavLink>
+      <nav className="flex justify-between items-center gap-4 p-4 h-16 sticky border-b bg-green-50">
+        <div className="flex items-center gap-2 text-green-500">
+          <Handbag className="size-8" strokeWidth={2.5} />
+        </div>
+        <div className="flex items-center gap-4">
+          <NavLink className="hover:text-green-500" to="/">
+            Home
+          </NavLink>
+          <NavLink className="hover:text-green-500" to="/products">
+            Products
+          </NavLink>
+          <NavLink className="hover:text-green-500" to="/cart">
+            Cart
+          </NavLink>
+          <NavLink className="hover:text-green-500" to="/about">
+            About
+          </NavLink>
+        </div>
+        <div className="hover:text-green-500">
+          <Link to="/profile">
+            <CircleUserRound className="size-10" strokeWidth={1.2} />
+          </Link>
+        </div>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
